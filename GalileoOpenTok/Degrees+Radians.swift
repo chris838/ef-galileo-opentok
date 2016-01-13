ae1f07571d3a70cd7991d5ac9c840cd7496c186b
Chris Harding <charding@palantir.com>
2016-01-13 12:00:34 +0000

    Added degress and radians utils.

---
+++ GalileoOpenTok/Degrees+Radians.swift
@@ -0,0 +1,20 @@
+
+import Foundation
+
+extension Int {
+    var degreesToRadians : Double {
+        return Double(self) * Double(M_PI) / 180.0
+    }
+}
+
+extension CGFloat {
+    var degreesToRadians : Double {
+        return Double(self) * Double(M_PI) / 180.0
+    }
+}
+
+extension Double {
+    var degreesToRadians : Double {
+        return Double(self) * Double(M_PI) / 180.0
+    }
+}
